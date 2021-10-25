fd4f4f040c2093909142e0b9aede184b85a8aec1
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-10-25 16:37:40 +0200

    Added registration form html

--- registration-form/src/components/FormSignIn.js
+++ registration-form/src/components/FormSignIn.js
@@ -3,7 +3,32 @@ import React from "react";
 export default function FormSignIn() {
   return (
     <div className='FormSignIn'>
-      <h1>I'm a form sign in</h1>
+      <form>
+        <div className="form-email">
+          <label htmlFor="email">Email</label>
+          {/* added to highlight on label click */}
+          <input
+            type="email"
+            name="email"
+            placeholder="test@test.de"
+            id="email"
+            required
+          />
+        </div>
+        <div className="form-password">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="********"
+            id="password"
+            required
+          />
+        </div>
+        <div className="form-submit">
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
     </div>
   );
 }
