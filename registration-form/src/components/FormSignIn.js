76717da89240ace3f4b7f2180e1dfd4067b446e2
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-10-25 16:54:26 +0200

    Added password validations

--- registration-form/src/components/FormSignIn.js
+++ registration-form/src/components/FormSignIn.js
@@ -18,6 +18,61 @@ export default function FormSignIn() {
     console.log("Submitting");
   }
 
+  let validations = {
+    characterscount: false,
+    lowercase: false,
+    uppercase: false,
+    decimal: false,
+    special: false
+  }
+
+  let validity = false;
+
+  // logic
+  // 8 or more characters X
+  // 1 or more lowercase letters X
+  // 1 or more uppercase letters X
+  // 1 or more decimal characters X
+  // 1 or more special characters X
+
+  let pwd = values.password.trim();
+  if (pwd.length >= 8) {
+    console.log("✅ Password long engouh")
+    validations.characterscount = true;
+  }
+
+  if (/[a-z]/.test(pwd)) {
+    console.log("Password has a lowercase");
+    validations.lowercase = true;
+  }
+
+  if (/[A-Z]/.test(pwd)) {
+    console.log("Password has a Uppercase");
+    validations.uppercase = true;
+  }
+
+  if (/\d/.test(pwd)) {
+    console.log("Password has a Decimal");
+    validations.decimal = true;
+  }
+
+  if (/\W/.test(pwd)) {
+    console.log("Password has a special Charachter");
+    validations.special = true;
+  }
+
+  for (const check in validations) {
+    console.log(`${check}: ${validations[check]}`);
+  }
+
+
+  if (Object.values(validations).every(Boolean)) {
+    validity = true;
+  } else {
+    validity = false;
+  }
+
+
   return (
     <div className='FormSignIn'>
       <form onSubmit={handleSubmit}>
@@ -45,6 +100,15 @@ export default function FormSignIn() {
             values={values.password}
             onChange={handleChange}
           />
+        </div>
+        {/* replace by favicons */}
+        <div className="form-validations">
+          <p>{validations.characterscount ? "✔️" : "❌"} 8+ characters</p>
+          <p>{validations.lowercase ? "✔️" : "❌"} lowercase letter</p>
+          <p>{validations.uppercase ? "✔️" : "❌"} uppercase letter</p>
+          <p>{validations.decimal ? "✔️" : "❌"} number</p>
+          <p>{validations.special ? "✔️" : "❌"} special character</p>
+          <p>{validity ? "✔️" : "❌"} All valid</p>
         </div>
         <div className="form-submit">
           <input type="submit" value="Submit" />
